24cc1e78a4f0d33100123347db50de3a4924b3a4
Suraj Prem Chandran <60046648+SurajPrem05@users.noreply.github.com>
2023-05-12 08:09:54 -0400

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,27 @@
+let lastScrollY = 0;
+let isScrollingDown = true;
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+  isScrollingDown = currentScrollY > lastScrollY;
+  lastScrollY = currentScrollY;
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const scrollUpIntoView = entry.isIntersecting && !isScrollingDown;
+    const scrollDownIntoView = entry.isIntersecting && isScrollingDown;
+    const scrollUpOutOfView = !entry.isIntersecting && !isScrollingDown;
+    const scrollDownOutOfView = !entry.isIntersecting && isScrollingDown;
+    if (scrollUpIntoView || scrollDownOutOfView || scrollDownIntoView) {
+      entry.target.classList.add('show')
+    } else if (scrollUpOutOfView) {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.animate-hidden-text, .animate-hidden, .animate-hidden-card')
+hiddenElements.forEach((element) => {
+  observer.observe(element)
+})
